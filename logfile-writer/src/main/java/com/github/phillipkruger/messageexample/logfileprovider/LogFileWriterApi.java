208db1d097b23f4b7916d3b8f96ff8969cae0c07
Phillip Kruger <phillip.kruger@gmail.com>
2017-11-03 08:53:10 +0200

    changed to allow level

--- logfile-writer/src/main/java/com/github/phillipkruger/messageexample/logfileprovider/LogFileWriterApi.java
+++ logfile-writer/src/main/java/com/github/phillipkruger/messageexample/logfileprovider/LogFileWriterApi.java
@@ -1,5 +1,6 @@
 package com.github.phillipkruger.messageexample.logfileprovider;
 
+import java.util.logging.Level;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -9,7 +10,7 @@ import javax.ws.rs.core.MediaType;
 import lombok.extern.java.Log;
 
 /**
- * JAX-RS 2.0, JAX-WS 2.2, JAXB 2.2, Bean validation 1.1
+ * JAX-RS 2.0, Bean validation 1.1
  * 
  * The API for outside consumption.  
  * Print to the log file
@@ -22,9 +23,10 @@ import lombok.extern.java.Log;
 public class LogFileWriterApi {
     
     @POST
-    @Path("{message}")
-    public void log(@NotNull @PathParam("message") String message) {
-        log.info(message);
+    @Path("{level}")
+    public void log(@NotNull @PathParam("level")String level, String message) {
+        Level l = Level.parse(level);
+        log.log(l, ">>>>>>>>> {0}", message);
     }
     
 }
